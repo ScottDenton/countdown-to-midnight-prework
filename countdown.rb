e33946cc709cb20fb6b3719c518843bc8c881450
scottyd_14 <scottyd_14@hotmail.com>
2018-10-03 18:21:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -13,10 +13,11 @@ end
 
 def countdown_with_sleep(num, rest)
   while num > 0 do
-    puts "#{num} SECOND(S)!"
     sleep #{rest}
+    puts "#{num} SECOND(S)!"
     num -=1 
   end
+  
   puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
 end
